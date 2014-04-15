d9279bc9aec44b2a31244ec9e15a0f8d4fb0f0c6
Ken Grønnbeck <kengroenn@gmail.com>
2014-04-15 20:22:58 +0200

    (chore) Stripped away non-magrathea code

--- bin/proxy.js
+++
@@ -1,8 +0,0 @@
-var proxy = require('../modules/proxy/proxy.js')
- , _ = require('underscore')
- , parseConfig = require('../modules/parse-config.js')
- , argv = process.argv
-
-var config = parseConfig.parse(argv)
-
-proxy.start(config)
