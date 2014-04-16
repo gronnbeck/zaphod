e53b061badea3895adc405d146fd8058623a6596
Ken Grønnbeck <kengroenn@gmail.com>
2014-04-16 20:42:36 +0200

    (refactor) Removed logic for bin/zaphod into modules/zaphod

--- bin/zaphod.js
+++ bin/zaphod.js
@@ -1,55 +1,24 @@
 #!/usr/local/bin/node
 
-var WebSocket = require('ws')
-	, _ = require('underscore')
-	, GetOpt = require('node-getopt')
-	, getopt = new GetOpt([
-			['p', 'port='],
-			['u', 'url=']
-		])
-	, argv = process.argv
-	, opt = getopt.parse(argv)
-  , defaults = {
-		url: 'ws://localhost',
-		port: 8080
-	}
- , config = _.defaults(opt.options, defaults)
- , keyPath = 'zaphod.key'
+var _ = require('underscore')
+, GetOpt = require('node-getopt')
+, zaphod = require('../modules/zaphod')
+, getopt = new GetOpt([
+		['p', 'port='],
+		['u', 'url=']
+	])
+, opt = getopt.parse(process.argv)
+, defaults = {
+	url: 'ws://localhost',
+	port: 8080
+}
+, config = _.defaults(opt.options, defaults)
+
 
 console.log('Starting bot with config: ')
 console.log(JSON.stringify(config, null, 4))
 
 var url = config.url + ':' + config.port
-console.log('Establishing connection to ' + url)
-var ws = new WebSocket(url);
+, keyPath = 'zaphod.key'
 
-var Handlers = require('../modules/handlers')
- , Connection = require('../modules/connection')
- , handlers = Handlers.init(ws)
- , connection = Connection.init(ws)
-
-
-ws.on('open', function() {
-	console.log('[ws] ws discovered');
-	connection.connect(keyPath)
-});
-
-ws.on('message', function(data) {
-	var message = JSON.parse(data);
-
-	if (message.success == false && message.type == 'disconnected') {
-		connection.reconnect()
-	}
-
-	else if (message.type == 'connected') {
-		connection.connected(keyPath, message.key)
-	}
-	else if (message.type == 'msg') {
-		handlers.userCommand(message)
-	}
-
-});
-
-ws.on('close', function() {
-	console.log('ws closed. Handle reconnection.');
-});
+zaphod.connect(url, keyPath)
---
+++ modules/zaphod.js
@@ -0,0 +1,38 @@
+var WebSocket = require('ws')
+, Handlers = require('../modules/handlers')
+, Connection = require('../modules/connection')
+
+exports.connect = function(url, keyPath) {
+
+  console.log('Establishing connection to ' + url)
+  var ws = new WebSocket(url);
+
+  var handlers = Handlers.init(ws)
+  , connection = Connection.init(ws)
+
+  ws.on('open', function() {
+    console.log('[ws] ws discovered');
+    connection.connect(keyPath)
+  });
+
+  ws.on('message', function(data) {
+    var message = JSON.parse(data);
+
+    if (message.success == false && message.type == 'disconnected') {
+      connection.reconnect()
+    }
+
+    else if (message.type == 'connected') {
+      connection.connected(keyPath, message.key)
+    }
+    else if (message.type == 'msg') {
+      handlers.userCommand(message)
+    }
+
+  });
+
+  ws.on('close', function() {
+    console.log('ws closed. Handle reconnection.');
+  });
+
+}
