504377070a1df835a95b6b774e9ba471dbfc1341
Ken Grønnbeck <kengroenn@gmail.com>
2014-04-17 15:36:40 +0200

    (feature) Dynamic script loader based on convention
    Any scripts added in scripts-folder will be loaded
    in the scriptLoader

--- modules/scriptLoader.js
+++ modules/scriptLoader.js
@@ -1,6 +1,29 @@
+var fs = require('fs')
+, _ = require('underscore')
+
+var isJavascript = function(file) {
+  return file.indexOf('.js') != -1
+}
+
+var scriptFiles = function (dir) {
+  try {
+    return _.filter(fs.readdirSync(dir), function(file) {
+      return isJavascript(file)
+    })
+  } catch (e) {
+    console.log('Could not find directory: ' + dir)
+    return []
+  }
+}
+
+var loadLocalScripts = function() {
+  var dir = 'scripts'
+  var files = scriptFiles(dir)
+  return _.map(files, function(file) {
+      return require('../' + dir + '/' + file)
+  })
+}
+
 exports.load = function() {
-  return [
-    require('../scripts/nick'),
-    require('../scripts/shut-up-marvin')
-  ]
+  return loadLocalScripts()
 }
